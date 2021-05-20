fe8be18da60e9b7a0118d65feb06f92d630bc886
The Unus Annus Archive <github@unusannusarchive.tk>
2021-05-20 17:44:58 -0500

    Add files via upload

--- build-mac.sh
+++ build-mac.sh
@@ -1,11 +1,29 @@
+[ ! -d "./dist/" ] && mkdir ./dist/
+
+echo "Building .app file..."
 npx electron-packager ./ --platform=darwin
 cd "The Unus Annus Archive Downloader-darwin-x64/"
+
+echo "Packing app into asar..."
+npx asar pack "./The Unus Annus Archive Downloader.app/Contents/Resources/app/" "./The Unus Annus Archive Downloader.app/Contents/Resources/app.asar"
+rm -rf "./The Unus Annus Archive Downloader.app/Contents/Resources/app/"
+
+echo "Cleaning up..."
 rm LICENSE
 rm LICENSES.chromium.html
 rm version
+
+echo "Building .dmg file..."
 ln -s /Applications Applications
-genisoimage -V "TUAA-Downloader" -D -R -apple -no-pad -o "The Unus Annus Archive Downloader.dmg" ./
-mkdir ../dist/
-mv "The Unus Annus Archive Downloader.dmg" ../dist/
+if [[ $(uname) == 'Darwin' ]]
+then
+    hdiutil makehybrid -o "../dist/The Unus Annus Archive Downloader.dmg" -hfs -default-volume-name TUAA-Downloader ./
+else
+    genisoimage -V "TUAA-Downloader" -D -R -apple -no-pad -o "../dist/The Unus Annus Archive Downloader.dmg" ./
+fi
+
+echo "Cleaning up..."
 cd ../
 rm -rf "The Unus Annus Archive Downloader-darwin-x64/"
+
+echo "Finished"
