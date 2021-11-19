8b796f388e7456204b489de0d8144a27f456538e
ScribbleNerd <contact@scribblenerd.com>
2021-11-18 22:28:30 -0600

    Update build-mac.sh

--- build-mac.sh
+++ build-mac.sh
@@ -1,29 +1,4 @@
-[ ! -d "./dist/" ] && mkdir ./dist/
-
-echo "Building .app file..."
-npx electron-packager ./ --platform=darwin
-cd "The Unus Annus Archive Downloader-darwin-x64/"
-
-echo "Packing app into asar..."
-npx asar pack "./The Unus Annus Archive Downloader.app/Contents/Resources/app/" "./The Unus Annus Archive Downloader.app/Contents/Resources/app.asar"
-rm -rf "./The Unus Annus Archive Downloader.app/Contents/Resources/app/"
-
-echo "Cleaning up..."
-rm LICENSE
-rm LICENSES.chromium.html
-rm version
-
-echo "Building .dmg file..."
-ln -s /Applications Applications
-if [[ $(uname) == 'Darwin' ]]
-then
-    hdiutil makehybrid -o "../dist/The Unus Annus Archive Downloader.dmg" -hfs -default-volume-name TUAA-Downloader ./
-else
-    genisoimage -V "TUAA-Downloader" -D -R -apple -no-pad -o "../dist/The Unus Annus Archive Downloader.dmg" ./
-fi
-
-echo "Cleaning up..."
-cd ../
-rm -rf "The Unus Annus Archive Downloader-darwin-x64/"
-
-echo "Finished"
+npx electron-builder -m
+rm -rf dist/mac
+rm dist/builder-debug.yml
+rm dist/builder-effective-config.yaml
